f7863cb8060265842c0da33d39b2f8837b290e97
Nima <onyourownsam@gmail.com>
2024-05-03 13:43:46 +0330

    fix cors err

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -66,6 +66,9 @@ SIMPLE_JWT = {
     "TOKEN_OBTAIN_SERIALIZER": "store.api.serializers.MyTokenObtainPairSerializer",
 }
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
